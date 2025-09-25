78ea5a3014f3e4073fc7391feee40171d93e6ea7
Zain Rafique <154544841+ZainRafiqueDev@users.noreply.github.com>
2025-09-26 04:40:29 +0500

    Update vite.config.js

--- f1/vite.config.js
+++ f1/vite.config.js
@@ -1,27 +1,21 @@
-
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
-import tailwindcss from '@tailwindcss/vite'
-
-// Load .env into process.env
-dotenv.config();
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  define: {
-    // Shim process.env so you can keep using process.env.VITE_API_URL
-    "process.env": process.env,
-  },
+  plugins: [react(), tailwindcss()],
   server: {
-    port: 5173, // or whatever you prefer
+    port: 5173,
     proxy: {
       "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:5000",
+        target: import.meta.env.VITE_API_URL || "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
     },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
   },
 });
